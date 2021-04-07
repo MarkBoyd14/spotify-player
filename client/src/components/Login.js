b94ee1cd4c3c6e05115dccc4dae8c829475f18b7
Mark Boyd <mark.boyd1414@gmail.com>
2021-04-07 13:41:43 -0400

    styled the login page, added spotify logo and favicon

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SpotifyLogo from '../images/Spotify_Logo_RGB_Green.png';
 
 import { Container } from 'react-bootstrap';
 
@@ -11,10 +12,16 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=627af30b66ea4
 export default function Login() {
   return (
     <Container
-      className="d-flex justify-content-center align-items-center"
-      style={{ minHeight: '100vh' }}
+      fluid
+      className="d-flex justify-content-center align-items-center flex-column"
+      style={{ minHeight: '100vh', backgroundColor: '#121212' }}
     >
-      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+      <img src={SpotifyLogo} alt="Spotify logo" style={{ maxWidth: '460px' }} />
+      <a
+        className="btn btn-lg mt-2 login-btn"
+        href={AUTH_URL}
+        style={{ backgroundColor: '#1db954', color: '#f5f5f5' }}
+      >
         Login With Spotify
       </a>
     </Container>
