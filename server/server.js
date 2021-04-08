c1e4fcb844e58b2c99b62f6b51593d157ef715a4
Mark Boyd <mark.boyd1414@gmail.com>
2021-04-08 01:43:27 -0400

    fix proxy

--- server/server.js
+++ server/server.js
@@ -12,13 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV == 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
-
 app.post('/refresh', (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
@@ -68,6 +61,13 @@ app.get('/lyrics', async (req, res) => {
     'No Lyrics Found';
   res.json({ lyrics });
 });
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
