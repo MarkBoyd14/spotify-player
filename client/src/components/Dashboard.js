4f93a0e75987924af2da1472e6c68cf81ce1010f
Mark Boyd <mark.boyd1414@gmail.com>
2021-04-07 23:25:36 -0400

    fix proxy

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -26,7 +26,7 @@ export default function Dashboard({ code }) {
   useEffect(() => {
     if (!playingTrack) return;
     axios
-      .get('http://localhost:3001/lyrics', {
+      .get('/lyrics', {
         params: {
           track: playingTrack.title,
           artist: playingTrack.artist,
